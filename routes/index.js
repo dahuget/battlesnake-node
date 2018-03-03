87a862cb5054c0e8370831b5bd5d81aebd6f332f
noah-boulton <noah.boulton@gmail.com>
2018-03-03 12:02:27 -0800

    Added avoid walls

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,28 @@ var router  = express.Router()
 function snakeHead(snake) {
   return snake.body.data[0]; // {"x": 8, "y": 15 }
 }
+
+function avoidWalls(head, height, width) {
+  var moves = [true, true, true, true] //left, right, up, down
+  if(head.x -1 < 0){
+    //cant go left
+    moves[0] = false
+  }
+  if(head.x +1 >= width){
+    //cant fo right
+    moves[1] = false
+  }
+  if(head.y -1 < 0){
+    //cant go up
+    moves[2] = false
+  }
+  if(head.y +1 >= height){
+    //cant go down
+    moves[3] = false
+  }
+  return moves
+}
+
 // Handle POST request to '/start'
 router.post('/start', function (req, res) {
   // NOTE: Do something here to start the game
