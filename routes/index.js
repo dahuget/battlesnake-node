e0a08a82811f280547d621e4246ff86f76a5784b
Wesley <wes@metalab.co>
2018-03-03 13:03:49 -0800

    fixfixfix

--- routes/index.js
+++ routes/index.js
@@ -69,25 +69,6 @@ function avoidSenks(data, head, moveOptions) {
       }
     }
   }
-
-  for (i = 1; i < body.length; i++){
-    if(head.x -1 == body[i].x && head.y == body[i].y){
-      //cant go left
-      moveOptions[0] = false
-    }
-    if(head.x +1 == body[i].x && head.y == body[i].y){
-      //cant go right
-      moveOptions[1] = false
-    }
-    if(head.x == body[i].x && head.y -1 == body[i].y){
-      //cant go up
-      moveOptions[2] = false
-    }
-    if(head.x == body[i].x && head.y +1 == body[i].y){
-      //cant go down
-      moveOptions[3] = false
-    }
-  }
 }
 
 function pickMove(data, moveOptions) {
