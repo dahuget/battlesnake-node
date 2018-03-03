4eaa73bde49cd5ab4ef02e4c955003dfa6a7a19f
noah-boulton <noah.boulton@gmail.com>
2018-03-03 15:33:26 -0800

    Added array shuffle and find path

--- routes/index.js
+++ routes/index.js
@@ -91,6 +91,56 @@ function pickMove(data, moveOptions) {
 
 }
 
+function findPath(head, target){
+  var xMoves = target.x - head.x;
+  var yMoves = target.y - head.y;
+  var left = true;
+  var right = true;
+  var up = true;
+  var down = true;
+  if(xMoves < 0){
+    //moving left
+    right = false;
+  }
+  if(yMoves < 0){
+    //moving up
+    down = false
+  }
+  if(xMoves == 0){
+    right = false
+    left = false
+  }
+  if(yMoves == 0){
+    up = false
+    down = false
+  }
+  var horiz = [];
+  for(i = 0; i < Math.abs(xMoves); i++){
+    if(right)
+      horiz.push('right')
+    else if(left)
+      horiz.push('left')
+  }
+  var vert = [];
+  for(i = 0; i < Math.abs(yMoves); i++){
+    if(up)
+      horiz.push('up')
+    else if(down)
+      horiz.push('down')
+  }
+  var path = horiz.concat(vert)
+  shuffle(path)
+  return path
+}
+
+function shuffle(path) {
+    for (i = path.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        [path[i], path[j]] = [path[j], path[i]];
+    }
+    return path;
+}
+
 // find closest food point to head location
 function findFood(data) {
   var foodLocation = data.food.data
@@ -142,6 +192,7 @@ router.post('/move', function (req, res) {
     taunt: 'Outta my way, snake!!!', // optional, but encouraged!
     head: snakeHead(req.body.you),
     nearestFood: findFood(req.body)
+    var path = findPath(snakeHead(req.body.you, nearestFood))
   }
 
   return res.json(data)
